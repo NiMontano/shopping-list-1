b36cb194ab416be4cbc975ce748247b79ff16b5e
NiMontano <63131933+NiMontano@users.noreply.github.com>
2020-11-26 16:59:02 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+$(function() {
+  $("#js-shopping-list-form").submit(function () {
+    event.preventDefault();
+    const newItem = $('#shopping-list-entry').val();
+    $('.shopping-list').append(`<li> <span class="shopping-item">${newItem}</span> <div class="shopping-item-controls"> <button class="shopping-item-toggle"> 
+           <span class="button-label">check</span>     
+            </button>    
+              <button class="shopping-item-delete">      
+                <span class="button-label">delete</span>     
+                 </button>  
+                   </div> </li>`);
+    
+  });
+
+  $('.shopping-list').on('click', '.shopping-item-delete', function() {
+    $(this).closest('li').remove();  
+  });
+})
+
+
+  $('.shopping-list').on('click', '.shopping-item-toggle', function() {
+    $(this).closest('li').children('.shopping-item').toggleClass('shopping-item__checked');
+});
